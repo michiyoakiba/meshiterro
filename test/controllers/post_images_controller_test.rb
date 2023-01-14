86631dec7a41cb300e5381d4712b831d334833b3
EC2 Default User <ec2-user@ip-172-31-5-65.ap-northeast-1.compute.internal>
2023-01-14 13:21:22 +0000

    meshiterro(作業中-13章まで)

---
+++ test/controllers/post_images_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class PostImagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get post_images_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get post_images_index_url
+    assert_response :success
+  end
+
+  test "should get shownew" do
+    get post_images_shownew_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get post_images_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get post_images_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get post_images_edit_url
+    assert_response :success
+  end
+end
